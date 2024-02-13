7a66eec335592b251ad159c8c1205b2556c2e1aa
darch <damianarchala@gmail.com>
2024-02-14 00:32:46 +0100

    Move HttpClient bean to the DomainConfiguration

--- src/main/java/pl/archala/repositorysearcher/configuration/DomainConfiguration.java
+++ src/main/java/pl/archala/repositorysearcher/configuration/DomainConfiguration.java
@@ -5,11 +5,20 @@ import com.google.gson.GsonBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.http.HttpClient;
+
 @Configuration
 public class DomainConfiguration {
 
     @Bean
     public Gson gson() {
         return new GsonBuilder().create();
+    }
+
+    @Bean
+    public HttpClient httpClient() {
+        return HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .build();
     }
 }
