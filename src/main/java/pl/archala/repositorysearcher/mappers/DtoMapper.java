fe0076efe9d377812922106cc4d4707f696081eb
darch <damianarchala@gmail.com>
2024-02-16 00:15:56 +0100

    Add DtoMapper, RestClientExceptionProvider

---
+++ src/main/java/pl/archala/repositorysearcher/mappers/DtoMapper.java
@@ -0,0 +1,19 @@
+package pl.archala.repositorysearcher.mappers;
+
+import pl.archala.repositorysearcher.dto.BranchDTO;
+import pl.archala.repositorysearcher.dto.RepositoryDTO;
+import pl.archala.repositorysearcher.model.Branch;
+import pl.archala.repositorysearcher.model.Repository;
+
+import java.util.ArrayList;
+
+public final class DtoMapper {
+
+    public static Repository toRepository(RepositoryDTO repositoryDTO) {
+        return new Repository(repositoryDTO.name(), repositoryDTO.fork(), new ArrayList<>());
+    }
+
+    public static Branch toBranch(BranchDTO branchDTO) {
+        return new Branch(branchDTO.name(), branchDTO.commit().sha());
+    }
+}
