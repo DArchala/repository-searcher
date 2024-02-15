dbb9686cd2cbc6da2e98e3e15046195a082efe38
darch <damianarchala@gmail.com>
2024-02-15 21:15:41 +0100

    Add ParameterizedTypeReference classes

---
+++ src/main/java/pl/archala/repositorysearcher/typeReferences/UserRepoDTOType.java
@@ -0,0 +1,9 @@
+package pl.archala.repositorysearcher.typeReferences;
+
+import org.springframework.core.ParameterizedTypeReference;
+import pl.archala.repositorysearcher.dto.UserRepoDTO;
+
+import java.util.List;
+
+public class UserRepoDTOType extends ParameterizedTypeReference<List<UserRepoDTO>> {
+}
