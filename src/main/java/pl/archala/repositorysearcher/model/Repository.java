a0f22b1aa026c065295dec855fbb11d7b0c7e667
darch <damianarchala@gmail.com>
2024-02-16 20:28:09 +0100

    Add JsonIgnore to Repository fork field

--- src/main/java/pl/archala/repositorysearcher/model/Repository.java
+++ src/main/java/pl/archala/repositorysearcher/model/Repository.java
@@ -1,7 +1,9 @@
 package pl.archala.repositorysearcher.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.util.List;
 
-public record Repository(String name, boolean fork, List<Branch> branches) {
+public record Repository(String name, @JsonIgnore boolean fork, List<Branch> branches) {
 
 }
