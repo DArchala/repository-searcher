793ff0f17ae83e889c79a866bac150de2f2704d6
darch <damianarchala@gmail.com>
2024-02-18 23:52:18 +0100

    Move ResponseError creating to private method

--- src/main/java/pl/archala/repositorysearcher/configuration/RestExceptionHandler.java
+++ src/main/java/pl/archala/repositorysearcher/configuration/RestExceptionHandler.java
@@ -1,6 +1,7 @@
 package pl.archala.repositorysearcher.configuration;
 
 import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,25 +18,27 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ResponseError> handleUserNotFoundException(UserNotFoundException e) {
-        return getResponseEntityFromError(new ResponseError(NOT_FOUND, e.getMessage()));
+        return getResponseEntity(NOT_FOUND, e.getMessage());
     }
 
     @ExceptionHandler(RepositoriesNotFoundException.class)
     public ResponseEntity<ResponseError> handleRepositoriesNotFoundException(RepositoriesNotFoundException e) {
-        return getResponseEntityFromError(new ResponseError(NOT_FOUND, e.getMessage()));
+        return getResponseEntity(NOT_FOUND, e.getMessage());
     }
 
     @ExceptionHandler(InternalServerException.class)
     public ResponseEntity<ResponseError> handleInternalServerException(InternalServerException e) {
-        return getResponseEntityFromError(new ResponseError(INTERNAL_SERVER_ERROR, e.getMessage()));
+        return getResponseEntity(INTERNAL_SERVER_ERROR, e.getMessage());
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ResponseError> handleConstraintViolationException(ConstraintViolationException e) {
-        return getResponseEntityFromError(new ResponseError(BAD_REQUEST, e.getConstraintViolations().iterator().next().getMessageTemplate()));
+        return getResponseEntity(BAD_REQUEST, e.getConstraintViolations().iterator().next().getMessageTemplate());
     }
 
-    private ResponseEntity<ResponseError> getResponseEntityFromError(ResponseError responseError) {
-        return ResponseEntity.status(responseError.status().value()).body(responseError);
+    private ResponseEntity<ResponseError> getResponseEntity(HttpStatus status, String message) {
+        ResponseError error = new ResponseError(status, message);
+        return ResponseEntity.status(error.status().value()).body(error);
     }
+
 }
